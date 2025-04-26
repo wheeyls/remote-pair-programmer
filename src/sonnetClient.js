adab927965939fcff132693a8861e1635066bf34
Michael Wheeler <mwheeler@g2crowd.com>
2025-04-26 11:42:23 -0500

    Switch to openai

--- src/sonnetClient.js
+++
@@ -1,4 +0,0 @@
-// This file has been moved to src/aiAdapters/sonnet.js
-// This file is kept for backward compatibility but will be removed in a future version
-const SonnetAdapter = require('./aiAdapters/sonnet');
-module.exports = SonnetAdapter;
