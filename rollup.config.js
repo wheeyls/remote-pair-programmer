9975bccd7281b5b76a9a3f53d6cc3dd566ab7ec9
Michael Wheeler <mike@g2.com>
2025-04-28 22:17:11 -0500

    Add rollup to support js action (#90)
    * The changes look good. This configuration will help bundle your GitHub Action using Rollup, resolving dependencies and creating a single distributable file.
    
    Key points in the changes:
    1. Added Rollup configuration to bundle the code
    2. Updated package.json to include build script and Rollup dependencies
    3. Created a GitHub workflow to automatically build and test the action
    4. Updated action.yml and package.json to point to the bundled dist/index.js
    
    To complete the setup, run:
    ```bash
    npm install
    npm run build
    ```
    
    This should resolve the dependency issues and prepare your GitHub Action for deployment. The build process will:
    - Bundle your code
    - Resolve dependencies
    - Create a single distributable file
    - Keep @actions/core and @actions/github as external dependencies
    
    Would you like me to explain any part of the configuration in more detail?
    
    * Add rollup to support js action

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/handler.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: false,
+    inlineDynamicImports: true,
+  },
+  external: ['@actions/core', '@actions/github'],
+  plugins: [
+    nodeResolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    json(),
+  ],
+};
