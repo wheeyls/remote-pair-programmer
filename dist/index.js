bc4234c90e7e8d8a2ad39174205375dedc4f6efc
Michael Wheeler <mwheeler@g2crowd.com>
2025-04-29 08:55:13 -0500

    Update defaults

--- dist/index.js
+++ dist/index.js
@@ -49022,7 +49022,7 @@ const config = {
 
   // Queue Configuration
   queue: {
-    serviceUrl: process.env.QUEUE_SERVICE_URL,
+    serviceUrl: process.env.QUEUE_SERVICE_URL || 'https://www.remembotron.com/api/v1/command_requests',
     authToken: process.env.QUEUE_AUTH_TOKEN,
   },
 
